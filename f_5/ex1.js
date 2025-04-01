c6d58d91ca1f216334c926a7c0b43676da1546cf
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-04-01 17:15:03 +0100

    formulario+botao remover

---
+++ f_5/ex1.js
@@ -0,0 +1,69 @@
+let jogos = []
+
+const form = document.getElementById("form")
+
+function addtable(gameName, launchDate, gameType) {
+    let table = document.querySelector("#tabela")
+    table.innerHTML += `
+    <tr class="data">
+        <td>${gameName}</td>
+        <td>${launchDate}</td>
+        <td>${gameType}</td>
+        <td><button onclick="datalhesLinha(this)">Detalhes</button></td>
+        <td><button onclick="removerLinha(this)">Remover</button></td>
+    </tr>`
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value
+    const ano = new Date(document.getElementById('ano').value).getFullYear()
+    const genero = document.getElementById('genero').value
+    const avaliacao = +document.getElementById('avaliacao').value
+
+    const plataformas = [...document.getElementById('plataforma').selectedOptions].map(option => option.value)
+
+    const anoAtual = new Date().getFullYear()
+
+    if (ano < 0 || ano > anoAtual) {
+        alert("Por favor, preencha o ano corretamente!")
+        return
+    }
+
+    const novoJogo = { nome, ano, genero, plataformas, avaliacao }
+    jogos.push(novoJogo)
+    console.log(jogos)
+
+    addtable(nome, ano, genero)
+
+    form.reset()
+})
+
+function detalhesLinha(botao) {
+    const linha = botao.parentElement.parentElement;
+    const nome = linha.children[0].innerText;
+    const ano = parseInt(linha.children[1].innerText); 
+    const genero = linha.children[2].innerText;
+
+    const jogo = jogos.find(jogo => jogo.nome === nome && jogo.ano === ano && jogo.genero === genero);
+
+    if (jogo) {
+        alert(
+            `Detalhes do Jogo:\n` +
+            `Nome: ${jogo.nome}\n` +
+            `Ano: ${jogo.ano}\n` +
+            `Género: ${jogo.genero}\n` +
+            `Plataformas: ${jogo.plataformas.join(', ')}\n` +
+            `Avaliação: ${jogo.avaliacao}`
+        );
+    } else {
+        alert("Jogo não encontrado");
+    }
+}
+
+
+
+function removerLinha(botao) {
+    botao.parentElement.parentElement.remove()
+}
